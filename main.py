2a4ac7319faf8494f9f8531626f09410eba878b9
CjNuz <23399@students.mrgs.school.nz>
2024-05-15 14:48:54 +1200

    Made Buttons

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from tkinter import *
 
+#Main Page
 window = Tk()
 window.geometry("430x932")
 window.title ("SportsX")
+window.configure(background="black")
 
-canvas = Canvas(window, bg='black')
-canvas.pack ()
+#button functions
+def basketball():
+    print("You clicked the button!")
 
+#style
+
+#buttons
+button = Button(window, text= "🏀", command=basketball, font=("Roboto", 38))
+button.pack(pady=50)
+
+#run
 window.mainloop()
+
+#Basketball Page
+#Football Page
+#Cricket Page
+#images
+photo = PhotoImage()
