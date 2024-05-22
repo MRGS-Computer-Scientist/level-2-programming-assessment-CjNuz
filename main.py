881bd336b39594156b6543357bd8d8c608717f1e
CjNuz <23399@students.mrgs.school.nz>
2024-05-22 14:53:22 +1200

    Made Top Frame and Finished all 3 buttons

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 from tkinter import *
 
+
 #Main Page
 window = Tk()
 window.geometry("430x932")
 window.title ("SportsX")
 window.configure(background="black")
+window.resizable(width= False, height=False)
+
+#Frame
+top_frame = Frame( background= "grey", width=500, height=150)
+top_frame.pack()
+main_frame = Frame(background= "black", width=500, height=500)
+main_frame.pack()
 
 #button functions
+#Basketball Page
 def basketball():
     print("You clicked the button!")
+#Football Page
+def football():
+    print("Football")
+#Cricket Page
+def cricket():
+    print("cricket")
 
-# Load the image
-#basketball_image = PhotoImage(file=r"C:\Users\23399\OneDrive - Mt Roskill Grammar School\Pictures\basketball_logo_orange_ball.jpg")
-#style
-#image=basketball_image
+#Image
+Logo = PhotoImage (file = r"C:\Users\23399\OneDrive - Mt Roskill Grammar School\Pictures\Screenshots\sportsxlogo.jpg")
+#Image size
+Logo = Logo.subsample(3,3)
+
 #buttons
-button = Button(window, text= "🏀", command=basketball, font=("Roboto", 38), width=3, height=1, compound="left")
-button.pack(anchor="w", padx=20, pady=150)
-button = Button(window, text= "⚽", command=basketball, font=("Roboto", 38), width=3, height=1, compound="left")
-button.pack(anchor="w", pady=90, padx=150)
+basketball_button = Button(main_frame, text= "🏀", command=basketball, font=("Roboto", 38), width=3, height=1, compound="left")
+
+football_button = Button(main_frame, text= "⚽", command=football, font=("Roboto", 38), width=3, height=1, compound="right")
+
+cricket_button = Button(main_frame, text= "🏏", command=cricket, font=("Roboto", 38), width=3, height=1, compound="right")
+
+
+#button packs
+basketball_button.grid(row=0, column=0, padx=18, pady=150)
+football_button.grid(row=0, column=1, padx=30, pady=150)
+cricket_button.grid(row=0, column=2, padx=20, pady=150)
 
 #run
 window.mainloop()
 
-#Basketball Page
-#Football Page
-#Cricket Page
+
+
+
