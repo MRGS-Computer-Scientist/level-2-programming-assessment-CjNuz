2a78104b0a92c815ee1fca15ee2470a4fd131128
CjNuz <23399@students.mrgs.school.nz>
2024-05-23 13:48:29 +1200

    Made Logo and Organized Code

---
+++ app_settings.py
@@ -0,0 +1,15 @@
+#frame size
+frame_width = 500
+frame_height = 500
+
+#Image Frame size
+frameImg_width = 400
+frameImg_height = 150
+
+topF_height = 150
+#button size
+buttonY = 150
+button_width = 3
+button_height = 1
+
+
--- main.py
+++ main.py
@@ -1,51 +1,67 @@
 from tkinter import *
+from app_settings import *
+from os import * 
+from PIL import Image, ImageTk 
 
 
-#Main Page
-window = Tk()
-window.geometry("430x932")
-window.title ("SportsX")
-window.configure(background="black")
-window.resizable(width= False, height=False)
+class App():
+    def __init__(self):
+        # Main Page
+        window = Tk()
+        window.geometry("430x932")
+        window.title("SportsX")
+        window.configure(background="black")
+        window.resizable(width=False, height=False)
 
-#Frame
-top_frame = Frame( background= "grey", width=500, height=150)
-top_frame.pack()
-main_frame = Frame(background= "black", width=500, height=500)
-main_frame.pack()
+        # Frame
+        self.top_frame = Frame(window, background="black", width= frame_width, height= topF_height)
+        self.top_frame.pack_propagate(False)
+        self.main_frame = Frame(window, background="black", width= frame_width, height= frame_height)
 
-#button functions
-#Basketball Page
-def basketball():
-    print("You clicked the button!")
-#Football Page
-def football():
-    print("Football")
-#Cricket Page
-def cricket():
-    print("cricket")
+        # Frame pack
+        self.top_frame.pack()
+        self.main_frame.pack()
 
-#Image
-Logo = PhotoImage (file = r"C:\Users\23399\OneDrive - Mt Roskill Grammar School\Pictures\Screenshots\sportsxlogo.jpg")
-#Image size
-Logo = Logo.subsample(3,3)
+        # Button functions
+        # Basketball Page
+        def basketball():
+            print("You clicked the button!")
 
-#buttons
-basketball_button = Button(main_frame, text= "🏀", command=basketball, font=("Roboto", 38), width=3, height=1, compound="left")
+        # Football Page
+        def football():
+            print("Football")
 
-football_button = Button(main_frame, text= "⚽", command=football, font=("Roboto", 38), width=3, height=1, compound="right")
+        # Cricket Page
+        def cricket():
+            print("cricket")
 
-cricket_button = Button(main_frame, text= "🏏", command=cricket, font=("Roboto", 38), width=3, height=1, compound="right")
+        # Image
+        dirname = path.dirname(__file__)
+        filename = path.join(dirname, r'Images\sportsxlogo.jpg')
 
-
-#button packs
-basketball_button.grid(row=0, column=0, padx=18, pady=150)
-football_button.grid(row=0, column=1, padx=30, pady=150)
-cricket_button.grid(row=0, column=2, padx=20, pady=150)
-
-#run
-window.mainloop()
+        # Load image
+        self.image = Image.open(filename)
+        # Resize image to fit the top frame
+        self.image = self.image.resize((frameImg_width, frameImg_height), Image.LANCZOS)
+        self.Logo = ImageTk.PhotoImage(self.image)
 
 
+        # Background image label
+        self.backgroundlogo = Label(self.top_frame, image=self.Logo)
+        self.backgroundlogo.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # Buttons
+        basketball_button = Button(self.main_frame, text="🏀", command=basketball, font=("Roboto", 38), width=button_width, height=button_height, compound="left")
+        football_button = Button(self.main_frame, text="⚽", command=football, font=("Roboto", 38), width=button_width, height=button_height, compound="right")
+        cricket_button = Button(self.main_frame, text="🏏", command=cricket, font=("Roboto", 38), width=button_width, height=button_height, compound="right")
 
+        # Button packs
+        basketball_button.grid(row=0, column=0, padx=18, pady=buttonY)
+        football_button.grid(row=0, column=1, padx=30, pady=buttonY)
+        cricket_button.grid(row=0, column=2, padx=20, pady=buttonY)
+
+        # Run
+        window.mainloop()
+
+# Instantiate the app
+app = App()
